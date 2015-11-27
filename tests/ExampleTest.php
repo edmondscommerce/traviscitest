9fe859fe4534e25439d326def6fce42e3ebef5e2
Michael <michael@edmonndscommerce>
2015-11-27 16:20:07 +0000

    initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Edmondscommerce\Travisci\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true, "some blah");
+    }
+}
